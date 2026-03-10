e1192e367b0eaaaf90c52d9dd5eafc14f41c5ad1
srikruthi-7 <kruthicholluri06@gmail.com>
2026-03-10 22:35:40 +0530

    Implement model loading with gdown for MRI detection
    Added model loading functionality with download support.

--- app.py
+++ app.py
@@ -2,14 +2,31 @@ import streamlit as st
 import numpy as np
 import tensorflow as tf
 from PIL import Image
+import gdown
+import os
 
 st.set_page_config(page_title="Brain Tumor Detection", layout="centered")
 
 st.title("Brain Tumor Detection using CNN")
 st.write("Upload an MRI image to detect tumor presence.")
 
-# Load trained model
-model = tf.keras.models.load_model("brain_tumor_cnn.h5", compile=False)
+# ---------- MODEL DOWNLOAD + LOAD ----------
+
+@st.cache_resource
+def load_model():
+
+    model_path = "brain_tumor_cnn.h5"
+
+    if not os.path.exists(model_path):
+        url = "https://drive.google.com/uc?id=1q2FExxziI_dA5h1woBjyj-bBHs49fyVL"
+        gdown.download(url, model_path, quiet=False)
+
+    model = tf.keras.models.load_model(model_path, compile=False)
+    return model
+
+model = load_model()
+
+# ---------- IMAGE UPLOAD ----------
 
 uploaded_file = st.file_uploader("Upload MRI Image", type=["jpg","png","jpeg"])
 
